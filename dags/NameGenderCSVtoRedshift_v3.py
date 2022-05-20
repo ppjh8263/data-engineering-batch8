d45d1056082885dd6dd19fad3f36d612497f97fe
Keeyong (Max) <keeyonghan@hotmail.com>
2022-05-20 10:12:18 -0700

    Update NameGenderCSVtoRedshift_v3.py

--- dags/NameGenderCSVtoRedshift_v3.py
+++ dags/NameGenderCSVtoRedshift_v3.py
@@ -41,8 +41,6 @@ def load(**context):
     
     cur = get_Redshift_connection()
     lines = context["task_instance"].xcom_pull(key="return_value", task_ids="transform")
-    lines = iter(lines)
-    next(lines)
     sql = "BEGIN; DELETE FROM {schema}.{table};".format(schema=schema, table=table)
     for line in lines:
         if line != "":
